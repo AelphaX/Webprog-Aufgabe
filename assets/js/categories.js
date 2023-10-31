f334442790bb80ebfde194336b8e0879fa5b3997
AelphaX <144533036+AelphaX@users.noreply.github.com>
2023-10-31 09:58:17 +0100

    n.v.g.

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,42 +1,40 @@
 // Funktion zum Laden der Kategorien mithilfe von Fetch-API
 function loadCategories() {
     fetch('https://dummyjson.com/products')
-    .then(res => res.json())
-    .then(data => {
-        // Extrahiere eindeutige Kategorien aus den Produktdaten
-        const categories = new Set(data.products.map(product => product.category));
-        const sideNav = document.getElementById('sideNav'); // Holen Sie sich das Seiten-Navigations-Element
+        .then(res => res.json())
+        .then(data => {
+            // Extrahiere eindeutige Kategorien aus den Produktdaten
+            const categories = new Set(data.products.map(product => product.category));
+            const sideNav = document.getElementById('sideNav');
 
-        // Schleife durch die Kategorien und erstelle Links im Seiten-Navigationsmenü
-        categories.forEach(category => {
-            const categoryLink = document.createElement('a');
-            categoryLink.innerText = capitalizeFirstLetter(category); // Setze den Kategorienamen (erste Buchstabe großgeschrieben) als Text
-            categoryLink.href = "#"; // Setze den Link auf "#" (kann später angepasst werden)
-            categoryLink.onclick = function() {
-                filterProductsByCategory(category); // Füge einen Klick-Handler hinzu, um Produkte nach Kategorie zu filtern
-            };
-            const placeholder = document.getElementById('categoryPlaceholder'); // Hole das Platzhalter-Element
-            sideNav.insertBefore(categoryLink, placeholder); // Füge den Link vor den Platzhalter ein
+            // Schleife durch die Kategorien und erstelle Links im Seiten-Navigationsmenü
+            categories.forEach(category => {
+                const categoryLink = document.createElement('a');
+                categoryLink.innerText = capitalizeFirstLetter(category);
+                categoryLink.href = "#";
+                categoryLink.onclick = function () {
+                    filterProductsByCategory(category);
+                };
+                const placeholder = document.getElementById('categoryPlaceholder'); // Hole das Platzhalter-Element
+                sideNav.insertBefore(categoryLink, placeholder); // Füge den Link vor den Platzhalter ein
+            });
         });
-    });
 }
 
 // Funktion zum Filtern von Produkten nach Kategorie
 function filterProductsByCategory(category) {
     fetch('https://dummyjson.com/products')
-    .then(res => res.json())
-    .then(data => {
-        // Filtere Produkte nach der ausgewählten Kategorie
-        const filteredProducts = data.products.filter(product => product.category === category);
-        displayProducts(filteredProducts); // Zeige die gefilterten Produkte an
-    });
+        .then(res => res.json())
+        .then(data => {
+            const filteredProducts = data.products.filter(product => product.category === category);
+            displayProducts(filteredProducts);
+        });
 }
 
 // Funktion zum Anzeigen der Produkte
 function displayProducts(products) {
     let resultsHtml = '';
 
-    // Schleife durch die Produkte und erstelle HTML für jedes Produkt
     products.forEach(product => {
         resultsHtml += `
             <div class="product-searchlist" onclick="showDetail(${product.id})">
@@ -51,14 +49,13 @@ function displayProducts(products) {
     if (resultsHtml === '') {
         resultsHtml = '<div class="error-message">Keine Produkte gefunden.</div>';
     } else {
-        document.getElementById('searchResults').classList.add('has-results'); // Füge eine CSS-Klasse hinzu, wenn Ergebnisse vorhanden sind
+        document.getElementById('searchResults').classList.add('has-results');
     }
 
-    document.getElementById('searchResults').innerHTML = resultsHtml; // Füge das generierte HTML zu den Suchergebnissen hinzu
+    document.getElementById('searchResults').innerHTML = resultsHtml;
 }
 
-// Warte auf das Laden der DOM-Struktur, bevor die Kategorien geladen werden
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     loadCategories();
 });
 
@@ -70,32 +67,31 @@ function capitalizeFirstLetter(string) {
 // Funktion zum Laden der Kategorien mit einem repräsentativen Produkt
 function loadCategoriesS() {
     fetch('https://dummyjson.com/products')
-    .then(res => res.json())
-    .then(data => {
-        // Extrahiere eindeutige Kategorien aus den Produktdaten
-        const uniqueCategories = Array.from(new Set(data.products.map(product => product.category)));
-        
-        let resultsHtml = '';
+        .then(res => res.json())
+        .then(data => {
+            const uniqueCategories = Array.from(new Set(data.products.map(product => product.category)));
 
-        // Schleife durch die eindeutigen Kategorien und erstelle Kategorie-Elemente mit repräsentativen Bildern
-        uniqueCategories.forEach(category => {
-            const representativeProduct = data.products.find(product => product.category === category);
-            const representativeImage = representativeProduct ? representativeProduct.thumbnail : 'default-image-url.jpg';
+            let resultsHtml = '';
 
-            resultsHtml += `
+            // Schleife durch die eindeutigen Kategorien und erstelle Kategorie-Elemente mit repräsentativen Bildern
+            uniqueCategories.forEach(category => {
+                const representativeProduct = data.products.find(product => product.category === category);
+                const representativeImage = representativeProduct ? representativeProduct.thumbnail : 'default-image-url.jpg';
+
+                resultsHtml += `
             <div class="product-searchlist category-display" onclick="filterProductsByCategory('${category}')">
                 <img src="${representativeImage}" alt="${capitalizeFirstLetter(category)}" class="product-thumbnaillist">
                 <div class="product-infolist">
                     <button class="product-title-buttonlist">${capitalizeFirstLetter(category)}</button>
                 </div>
             </div>`;
-        });
+            });
 
-        document.getElementById('searchResults').innerHTML = resultsHtml; // Füge das generierte HTML zu den Suchergebnissen hinzu
-    });
+            document.getElementById('searchResults').innerHTML = resultsHtml; // Füge das generierte HTML zu den Suchergebnissen hinzu
+        });
 }
 
 // Warte auf das Laden der DOM-Struktur, bevor die Kategorien (mit repräsentativen Produkten) geladen werden
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     loadCategoriesS();
 });
